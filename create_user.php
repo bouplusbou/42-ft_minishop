78f65bbe4158b6ba66336ad1d12f5c310745cc19
Nestor Alonso <nalonso@student.42.fr>
2019-03-30 20:15:11 +0100

    new db workin

--- create_user.php
+++ create_user.php
@@ -1,56 +1,58 @@
 <?php
 
 include 'inc/functions_user.php';
+include 'inc/db_user_functions.php';
 
 $file = 'database/passwd';
 $errors = array();
-$db = unserialize_data($file);
+$passwd_db = unserialize_data($file);
 $errmsg = '';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if ($_POST['submit'] !== "Create") {
-		$errors[] = 'Invalid submit value';
-	}
+    if ($_POST['submit'] !== "Create") {
+        $errors[] = 'Invalid submit value';
+    }
 
-	if (strlen($_POST['email']) === 0) {
-		$errors[] = 'Please enter your email.';
-	} else if (is_valid_email($_POST['email']) == false) {
-		$errors[] = 'Invalid email adress format.';
-	}
+    if (strlen($_POST['email']) === 0) {
+        $errors[] = 'Please enter your email.';
+    } else if (is_valid_email($_POST['email']) == false) {
+        $errors[] = 'Invalid email adress format.';
+    }
 
-	if (strlen($_POST['passwd']) === 0) {
-		$errors[] = 'Please enter a password.';
-	}
+    if (strlen($_POST['passwd']) === 0) {
+        $errors[] = 'Please enter a password.';
+    }
 
-	if (check_user_existance($db, $_POST['email']) == true) {
-		$errors[] = 'There is already a user registered with that email.';
-	}
+    if (check_user_existance($passwd_db, $_POST['email']) == true) {
+        $errors[] = 'There is already a user registered with that email.';
+    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && count($errors) === 0) {
-	$db[] = create_user($_POST['email'], $_POST['passwd']);
-	serialize_data($db, $file);
-	header('Location: login.php');
+    $passwd_db[] = create_user($_POST['email'], $_POST['passwd']);
+    db_add_user('database/users', $_POST['email']);
+    serialize_data($passwd_db, $file);
+    header('Location: login.php');
 } else {
-	$errmsg = create_error_html($errors);
+    $errmsg = create_error_html($errors);
 }
 
 $css = "css/login.css";
 $title = "Create account";
 include 'inc/header.php';
 ?>
-	<div class="login-page">
-		<div class="form">
-			<form class="login-form" name="index.php" action="login.php" method="post">
-				<input placeholder="email address" type="text" value="" name="email" />
-				<input placeholder="password" type="password" value="" name="passwd" />
-				<input type="submit" value="Sign Up" name="submit" />
-			</form>
-			<?php if ($errmsg !== ''):
-				echo $errmsg;
-				endif; ?>
-			<a href="login.php">Sign In</a>
-		</div>
-	</div>
+<div class="login-page">
+    <div class="form">
+        <form class="login-form" name="index.php" action="create_user.php" method="post">
+            <input placeholder="email address" type="text" value="" name="email" />
+            <input placeholder="password" type="password" value="" name="passwd" />
+            <input type="submit" value="Create" name="submit" />
+        </form>
+        <?php if ($errmsg !== ''):
+            echo $errmsg;
+        endif; ?>
+        <a href="login.php">Sign In</a>
+    </div>
+</div>
 
 <?php include 'inc/footer.php'; ?>
---
+++ inc/db_user_functions.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * user_database[]:
+ *         - user_email
+ *         - previous cart if it exists that will become the cart when the user starts it's session
+ *         - array with all the previous orders
+ *                  - date
+ *                  - price
+ *                  - [product_id, ...]
+ */
+
+function db_add_user($path, $user_mail) {
+    $db = unserialize_data($path);
+
+    $db[] = ["user_mail" => $user_mail,
+        "cart" => array(),
+        "orders" => array()
+    ];
+
+    serialize_data($db, $path);
+}
+
+function db_delete_user($path, $user_mail) {
+    $db = unserialize_data($path);
+
+    foreach ($db as $key => $user) {
+        if ($user['user_mail'] == $user_mail) {
+            unset($db[$key]);
+            break ;
+        }
+    }
+    $db = array_values($db);
+    serialize_data($db, $path);
+}
+
+function db_new_order($date, $total_amount, $products) {
+    return ["date" => $date,
+        "total_amount" => $total_amount,
+        "items" => $products];
+}
+
+function db_get_orders($path, $user_mail) {
+    $db = unserialize_data($path);
+
+    foreach ($db as $key => $user) {
+        if ($user['user_mail'] == $user_mail) {
+            return $db[$key]["orders"];
+        }
+    }
+    return null;
+}
+
+function db_get_cart($path, $user_mail) {
+    $db = unserialize_data($path);
+
+    foreach ($db as $key => $user) {
+        if ($user['user_mail'] == $user_mail) {
+            return $db[$key]["cart"];
+        }
+    }
+    return null;
+}
+
+function db_set_cart($path, $user_mail, $new_cart) {
+    $db = unserialize_data($path);
+
+    foreach ($db as $key => $user) {
+        if ($user['user_mail'] == $user_mail) {
+            $db[$key]["cart"] = $new_cart;
+        }
+    }
+    serialize_data($db, $path);
+}
+
+
+function db_add_order($path, $user_mail, $last_oder) {
+    $db = unserialize_data($path);
+
+    foreach ($db as $key => $user) {
+        if ($user['user_mail'] == $user_mail) {
+            $db[$key]["orders"][] = $last_oder;
+            break ;
+        }
+    }
+    serialize_data($db, $path);
+}
