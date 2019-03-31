3498c69da2a84adbdb914d35cc4967b05c482e8c
Baptiste BOUCHER <bboucher@e2r6p12.42.fr>
2019-03-31 19:21:32 +0200

    fix my account admin

--- account.php
+++ account.php
@@ -9,12 +9,12 @@ include 'inc/header.php';
 <h1>Your Account</h1>
 <div class="account_container">
 	<?php if ($_SESSION['admin'] === true) {?>
-        <a class="manage_products" href="admin_products.php">Manage products</a>
-		<a class="manage_users" href="admin_users.php">Manage users</a>
-		<a class="manage_users" href="admin_orders.php">Manage all orders</a>
+        <a class="manage" href="admin_products.php">Manage products</a>
+		<a class="manage" href="admin_users.php">Manage users</a>
+		<a class="manage" href="admin_orders.php">Manage all orders</a>
 	<?php } ?>
-	<a class="change_password" href="user_orders.php">My orders</a>
-	<a class="change_password" href="modif.php">Change my password</a>
+	<a class="orders" href="user_orders.php">My orders</a>
+	<a class="password" href="modif.php">Change my password</a>
 	<a class="delete_account" href="delete_account.php">Delete my account</a>
 </div>
 
