eb39fc23e04a9910f3747caeea7f649a3e2cf481
Nestor Alonso <nalonso@student.42.fr>
2019-03-31 20:31:04 +0200

    modif css

--- modif.php
+++ modif.php
@@ -9,48 +9,50 @@ $db = unserialize_data($file);
 $errmsg = '';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if ($_POST['submit'] !== "Change") {
-		$errors[] = 'Invalid submit value';
-	}
+    if ($_POST['submit'] !== "Change") {
+        $errors[] = 'Invalid submit value';
+    }
 
-	if (strlen($_POST['oldpw']) === 0) {
-		$errors[] = 'Please enter your current password.';
-	}
+    if (strlen($_POST['oldpw']) === 0) {
+        $errors[] = 'Please enter your current password.';
+    }
 
-	if (strlen($_POST['newpw']) === 0) {
-		$errors[] = 'Please enter the new password.';
-	}
+    if (strlen($_POST['newpw']) === 0) {
+        $errors[] = 'Please enter the new password.';
+    }
 
-	if (check_user_existance($db, $_SESSION['username'], $_POST['oldpw']) == false) {
-		$errors[] = 'Incorrect current password';
-	}
+    if (check_user_existance($db, $_SESSION['username'], $_POST['oldpw']) == false) {
+        $errors[] = 'Incorrect current password';
+    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && count($errors) === 0) {
-	foreach ($db as $key => $user) {
-		if ($user['user'] == $_SESSION['username']) {
-			$db[$key]['passwd'] = password_hash($_POST['newpw'], PASSWORD_BCRYPT);
-		}
-	}
+    foreach ($db as $key => $user) {
+        if ($user['user'] == $_SESSION['username']) {
+            $db[$key]['passwd'] = password_hash($_POST['newpw'], PASSWORD_BCRYPT);
+        }
+    }
 
-	serialize_data($db, $file);
-	header('Location: account.php');
+    serialize_data($db, $file);
+    header('Location: account.php');
 } else {
-	$errmsg = create_error_html($errors);
+    $errmsg = create_error_html($errors);
 }
 
 $title = "My account";
-$css = "css/index.css";
+$css = "css/login.css";
 include 'inc/header.php';
 ?>
-<div class="flex-container">
-	<form name="index.php" action="modif.php" method="post">
-		<label for="passwd">Previous password: </label><input type="password" value="" name="oldpw" />
-		<label for="passwd">New password: </label><input type="password" value="" name="newpw" />
-		<input type="submit" value="Change" name="submit" />
-	</form>
-	<?php if ($errmsg !== ''):
-			echo $errmsg;
-	endif; ?>
+<div class="login-page">
+    <div class="form">
+        <form name="index.php" action="modif.php" method="post">
+            <input placeholder="Previous password" type="password" value="" name="oldpw" />
+            <input placeholder="New password" type="password" value="" name="newpw" />
+            <input type="submit" value="Change" name="submit" />
+        </form>
+        <?php if ($errmsg !== ''):
+            echo $errmsg;
+        endif; ?>
+    </div>
 </div>
 <?php include 'inc/footer.php'; ?>
