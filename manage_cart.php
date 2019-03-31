84fd1f0f3c1d4f7a740a83528dfd436964fd3d31
Nestor Alonso <nalonso@student.42.fr>
2019-03-31 14:08:18 +0200

    cart working

--- manage_cart.php
+++ manage_cart.php
@@ -4,12 +4,6 @@ $cart = unserialize($_COOKIE['cart']);
 
 if ($_POST['add']) {
 	$cart[] = ["product_id" => $_POST['product_id']];
-} elseif ($_POST['delete']) {
-	unset($cart[$_POST['product_id']]);
-} elseif ($_POST['plus']) {
-	var_dump($cart);
-} elseif ($_POST['minus']) {
-	var_dump($cart);
 }
 $serialized_cart = serialize($cart);
 setcookie("cart", $serialized_cart, time() + 86400);
