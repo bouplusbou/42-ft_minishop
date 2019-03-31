905bc1f283bef8e8d82046d73d71d898f1f30536
Baptiste BOUCHER <bboucher@e2r6p12.42.fr>
2019-03-31 16:03:08 +0200

    admin products OK

--- admin.php
+++ admin.php
@@ -5,60 +5,56 @@ $css = "./css/admin.css";
 include 'inc/functions_user.php';
 include 'inc/header.php';
 ?>
-	<h1>Admin Panel - Product</h1>
+	<h1>- Product Admin Panel -</h1>
 	<div class="add_product">
-		<h2>Add a product</h2>
-		<form name="index.php" action="manage_products.php" method="POST">
+		<h2>ADD A PRODUCT</h2>
+		<form name="index.php" name="add_product_form" action="manage_products.php" method="POST">
 			<input name="type" type="hidden" value="create">
-			<label for="product_name">Name </label><br>
-			<input type="text" value="" name="name" required><br>
-			<label for="product_price">Price </label><br>
-			<input type="number" value="" name="price" required><br>
-			<label for="product_photo">Photo url </label><br>
-			<input type="url" value="" name="img_url" required><br>
-			<?php $categories = get_categories();
-			foreach ($categories as $key => $category) { ?>
-			<input type="checkbox" id="<?=$category?>" name="category_<?=$key?>" value="<?=$category?>">
-  			<label for="<?=$category?>"><?=$category?></label>
-			<?php } ?>
-			<input type="submit" value="OK" name="submit">
+			<input placeholder="name" type="text" value="" name="name" required><br>
+			<input placeholder="price" type="number" value="" name="price" required><br>
+			<input placeholder="photo url" type="url" value="" name="img_url" required><br>
+			<div class="checkbox_wrapper">
+				<?php $categories = get_categories();
+				foreach ($categories as $key => $category) { ?>
+				<div class="checkbox">
+					<input type="checkbox" id="<?=$category?>" name="category_<?=$key?>" value="<?=$category?>">
+					<label for="<?=$category?>"><?=$category?></label>
+				</div>
+				<?php } ?>
+			</div>
+			<input type="submit" value="CREATE" name="submit">
 		</form>
-
 	</div>
-	
-	<p>Change a product</p>
-	<div class="wrapper">
-
+	<div class="separator"></div>
+	<div class="update_product">
+	<h2>UPDATE A PRODUCT</h2>
 		<?php $products = get_products();
 		foreach ($products as $product_id => $product) {?>
 		<div class="product_container">
-			<img src="<?=$product['img']?>" alt="" class="image">
-			<form name="index.php" action="manage_products.php" method="POST">
-				<input name="type" type="hidden" value="update">
-				<input name="product_id" type="hidden" value="<?=$product_id?>">
-				<label for="product_name">Name</label><br>
-				<input type="text" value="<?=$product['name']?>" name="name" ><br>
-				<label for="product_price">Price </label><br>
-				<input type="number" value="<?=$product['price']?>" name="price" ><br>
-				<label for="product_photo">Photo url </label><br>
-				<input type="text" value="<?=$product['img']?>" name="img_url"><br>
-				<?php $categories = get_categories();
-				foreach ($categories as $key => $category) { ?>
-				<input type="checkbox" id="<?=$category?>" name="category_<?=$key?>" value="<?=$category?>" <?=in_array($category, $product['categories']) ? "checked" : ""?>>
-  				<label for="<?=$category?>"><?=$category?></label>
-				<?php } ?>
-				<input type="submit" value="OK" name="submit">
-			</form>
-
-
-
-			
-			<div class="form-group">
-			  <form action="manage_products.php" method="POST">
-			  	<input name="type" type="hidden" value="delete">
-			    <input name="product_id" type="hidden" value="<?=$product_id?>"> 
-			    <button class="delete-button" type="submit">Delete product</button>
-			  </form>
+			<div class="product_img">
+				<img src="<?=$product['img']?>" alt="" class="image">
+			</div>
+			<div class="form_container">
+				<form name="index.php" action="manage_products.php" method="POST">
+					<input name="type" type="hidden" value="update">
+					<input name="product_id" type="hidden" value="<?=$product_id?>">
+					<input type="text" value="<?=$product['name']?>" name="name" ><br>
+					<input type="number" value="<?=$product['price']?>" name="price" ><br>
+					<input type="text" value="<?=$product['img']?>" name="img_url"><br>
+					<div class="checkbox_wrapper">
+						<?php $categories = get_categories();
+						foreach ($categories as $key => $category) { ?>
+						<input type="checkbox" id="<?=$category?>" name="category_<?=$key?>" value="<?=$category?>" <?=in_array($category, $product['categories']) ? "checked" : ""?>>
+						  <label for="<?=$category?>"><?=$category?></label>
+						<?php } ?>
+					</div>
+					<input type="submit" value="UPDATE" name="submit">
+				</form>
+				<form action="manage_products.php" method="POST">
+					<input name="type" type="hidden" value="delete">
+					<input name="product_id" type="hidden" value="<?=$product_id?>"> 
+					<input type="submit" value="DELETE" name="submit">
+				</form>
 			</div>
 		</div>
 		<?php } ?>
--- manage_products.php
+++ manage_products.php
@@ -2,44 +2,6 @@
 session_start();
 include 'inc/functions_user.php';
 
-function create_product($name, $price, $img_url, $categories) {
-	$products = get_products();
-	$img_path = download_img("./resources/products_img/", $img_url);
-	$product = array(
-		"name" => $name,
-		"price" => $price,
-		"img" => $img_path,
-		"categories" => $categories
-	);
-	$products[] = $product;
-	$products_serialized = serialize($products);
-	file_put_contents("./database/products", "$products_serialized", LOCK_EX);
-}
-
-function delete_product($product_id) {
-	$products = get_products();
-	unset($products[$product_id]);
-	$serialized_products = serialize($products);
-	file_put_contents("./database/products", "$serialized_products", LOCK_EX);
-}
-
-function update_product($product_id, $name, $price, $img_url, $categories) {
-	if (!preg_match("~^./resources/products_img/~", $img_url)) {
-		$img_url = download_img("./resources/products_img/", $img_url);
-	}
-	$products = get_products();
-	$product = array(
-		"name" => $name,
-		"price" => $price,
-		"img" => $img_url,
-		"categories" => $categories
-	);
-	$products[$product_id] = $product;
-	$products_serialized = serialize($products);
-	file_put_contents("./database/products", "$products_serialized", LOCK_EX);
-}
-
-
 $products = get_products();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if ($_POST['type'] === "create") {
@@ -58,6 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		update_product($_POST['product_id'], $_POST['name'], $_POST['price'], $_POST['img_url'], $categories);
 	}
 }
+
 $from = $_SERVER['HTTP_REFERER'];
 header("Location: $from");
 ?>	
